1a2c63ad4b5837b0725903505f62e223927882b8
Владислав Воронин <voronin@surfstudio.co>
2019-10-31 10:56:43 +0300

    no message

--- TaskParser.py
+++ TaskParser.py
@@ -7,3 +7,9 @@ def parser(*argv):
     for arg in argv:
         temp_url = url + arg
         webbrowser.get("safari").open_new_tab(temp_url)
+
+
+def default_browser_parser(*argv):
+    for arg in argv:
+        temp_url = url + arg
+        webbrowser.open_new_tab(temp_url)
